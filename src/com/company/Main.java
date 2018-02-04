c808f56d2b30fc676d0667d8e6441edea1ef9e8a
darekk <koncewicz.dariusz@gmail.com>
2018-02-04 23:37:00 +0100

    initial comit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hi, this is the simple command line program Web Walker");
+    }
+}
